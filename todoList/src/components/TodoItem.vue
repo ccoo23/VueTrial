9020c04b2cfb7cfa5d1c988e2a1414229ba23e75
ccoo23 <zyf527@vip.qq.com>
2018-10-09 17:56:14 +0800

    feat(*): add vue-cli

---
+++ todoList/src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <li @click="handleClick">{{content}} {{index}}</li>
+</template>
+
+<script>
+export default {
+    props: ['content', 'index'],
+    methods: {
+        handleClick () {
+            this.$emit('delete', this.index);
+        },
+    },
+}
+</script>
+
